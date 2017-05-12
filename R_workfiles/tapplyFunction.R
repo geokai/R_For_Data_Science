b0176bb87dc38a9fbdab3590f898610346b5fa6e
George Kaimakis <geomatus@mac.com>
2017-05-12 01:53:25 +0100

    Adds 'abline' plot, further work to tapplyFunction.R:

--- R_workfiles/tapplyFunction.R
+++ R_workfiles/tapplyFunction.R
@@ -5,6 +5,12 @@
 # Importing the data into a dataframe (variable)
 my_DF <- read.csv("/Users/geomatus3/1.Projects/•FutureLearn/Intro_to_R_for_Data_Science/datasets/2008.csv")
 
+# a listing of carriers
+carriers <- read.csv("../datasets/carriers.csv")
+
+# print the carriers
+carriers$Code["HA"]
+
 # printing to console the first 6 lines of the dataset
 head(my_DF)
 
@@ -57,10 +63,36 @@ head(sort(tapply(my_DF$Distance, my_DF$Dest, mean, na.rm=TRUE)))
 # 3. the function: mean
 # 4. allow for void values: na.rm=TRUE
 
-# for the mean departure delay
+# for the mean departure delay table
 mean(sort(round(tapply(my_DF$DepDelay, my_DF$Origin, mean, na.rm=TRUE))))
 
-# for the median departure delay
+# using 'abline()'
+abline(h=mean(tapply(my_DF$DepDelay, my_DF$Origin, mean, na.rm=TRUE)))
+?abline
+
+# for the mean departure delay plot
 plot(sort(tapply(my_DF$DepDelay, my_DF$Origin, mean, na.rm=TRUE)))
 
 
+# Which airlines have the best average Arrival Delays?
+# Which airlines have the worst average Arrival Delays?
+sort(tapply(my_DF$ArrDelay, my_DF$UniqueCarrier, mean, na.rm=TRUE))
+
+# Which airlines have the best average Departure Delays?
+# Which airlines have the worst average Departure Delays?
+sort(tapply(my_DF$DepDelay, my_DF$UniqueCarrier, mean, na.rm=TRUE))
+
+avgArrDly <- tapply(my_DF$ArrDelay, my_DF$UniqueCarrier, mean, na.rm=TRUE)
+plot(avgArrDly)
+axis(1, at = avgArrDly, labels = names(avgArrDly), las = 2)
+
+# and plot:
+plot(sort(tapply(my_DF$ArrDelay, my_DF$UniqueCarrier, mean, na.rm=TRUE)))
+
+# How many flights occur per month
+table(my_DF$Month)
+sort(tapply(my_DF$Month, my_DF$Month, length))
+
+# How many flights each carrier had in total
+sort(tapply(my_DF$UniqueCarrier, my_DF$UniqueCarrier, length))
+?tapply
