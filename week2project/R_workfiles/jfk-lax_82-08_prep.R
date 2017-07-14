d4368edf2a42a3c0432ee37cdd3412f2ecd8736f
George Kaimakis <geomatus@mac.com>
2017-07-14 23:04:22 +0100

    Amends to week 2 project files:

--- week2project/R_workfiles/jfk-lax_82-08_prep.R
+++ week2project/R_workfiles/jfk-lax_82-08_prep.R
@@ -7,33 +7,59 @@
 # 1988, 1992, 1996, 2000, 2004, 2008
 
 
-# Importing the data into a dataframe (variable) for each year
-main_DF <- read.csv("../../datasets/2008.csv")
+# Importing the data into a dataframe (variable) for each year (overwrite to avoid memory problems)
+main_DF <- read.csv("../../dl_datasets/2008.csv")
+main_DF <- read.csv("../../dl_datasets/2004.csv")
+main_DF <- read.csv("../../dl_datasets/2000.csv")
+main_DF <- read.csv("../../dl_datasets/1996.csv")
+main_DF <- read.csv("../../dl_datasets/1992.csv")
+main_DF <- read.csv("../../dl_datasets/1988.csv")
+
+# check to confirm the correct year
+head(main_DF)
+# remove to avoid memory problems
+rm(main_DF)
+
 # year 2008:
 # Create a subset of my_DF with Origin; JFK and Dest; LAX
-jfk_lax_2008 <- subset(main_DF, (my_DF$Origin == "JFK") & (my_DF$Dest == "LAX"))
+jfk_lax_2008 <- subset(main_DF, (main_DF$Origin == "JFK") & (main_DF$Dest == "LAX"))
 # 8078 obs, 29 var
+jfk_lax_2004 <- subset(main_DF, (main_DF$Origin == "JFK") & (main_DF$Dest == "LAX"))
+# 8514 obs, 29 var
+jfk_lax_2000 <- subset(main_DF, (main_DF$Origin == "JFK") & (main_DF$Dest == "LAX"))
+# 9277 obs, 29 var
+jfk_lax_1996 <- subset(main_DF, (main_DF$Origin == "JFK") & (main_DF$Dest == "LAX"))
+# 7954 obs, 29 var
+jfk_lax_1992 <- subset(main_DF, (main_DF$Origin == "JFK") & (main_DF$Dest == "LAX"))
+# 6248 obs, 29 var
+jfk_lax_1988 <- subset(main_DF, (main_DF$Origin == "JFK") & (main_DF$Dest == "LAX"))
+# 4963 obs, 29 var
 
-# Create a csv file of the (subset) new data frame and save it to the 'dataset' directory
-write.csv(jfk_lax, file = "../datasets/jfk-lax_2008/jfkTolax2008.csv", na = "NA")
+# check to confirm the correct year
+head(jfk_lax_2008)
+head(jfk_lax_2004)
+head(jfk_lax_2000)
+head(jfk_lax_1996)
+head(jfk_lax_1992)
+head(jfk_lax_1988)
 
 
-main_DF <- read.csv("../../datasets/2004.csv")
-# year 2004:
-# Create a subset of my_DF with Origin; JFK and Dest; LAX
-jfk_lax_2004 <- subset(main_DF, (my_DF$Origin == "JFK") & (my_DF$Dest == "LAX"))
-# .... obs, 29 var
+# Create a csv file of the (subset) 2008 data frame and save it to the 'dataset' directory
+write.csv(jfk_lax_2008, file = "../datasets/jfk-lax_2008/jfkTolax2008.csv", quote = FALSE, row.names = FALSE, na = "NA")
 
-# Create a csv file of the (subset) new data frame and save it to the 'dataset' directory
-write.csv(jfk_lax, file = "../datasets/jfk-lax_2004/jfkTolax2004.csv", na = "NA")
+# Create a csv file of the (subset) 2004 data frame and save it to the 'dataset' directory
+write.csv(jfk_lax_2004, file = "../datasets/jfk-lax_2004/jfkTolax2004.csv", quote = FALSE, row.names = FALSE, na = "NA")
 
+# Create a csv file of the (subset) 2000 data frame and save it to the 'dataset' directory
+write.csv(jfk_lax_2000, file = "../datasets/jfk-lax_2000/jfkTolax2000.csv", quote = FALSE, row.names = FALSE, na = "NA")
 
-main_DF <- read.csv("../../datasets/2000.csv")
-# year 2000:
-# Create a subset of my_DF with Origin; JFK and Dest; LAX
-jfk_lax_2000 <- subset(main_DF, (my_DF$Origin == "JFK") & (my_DF$Dest == "LAX"))
-# .... obs, 29 var
+# Create a csv file of the (subset) 1996 data frame and save it to the 'dataset' directory
+write.csv(jfk_lax_1996, file = "../datasets/jfk-lax_1996/jfkTolax1996.csv", quote = FALSE, row.names = FALSE, na = "NA")
 
-# Create a csv file of the (subset) new data frame and save it to the 'dataset' directory
-write.csv(jfk_lax, file = "../datasets/jfk-lax_2000/jfkTolax2000.csv", na = "NA")
+# Create a csv file of the (subset) 1992 data frame and save it to the 'dataset' directory
+write.csv(jfk_lax_1992, file = "../datasets/jfk-lax_1992/jfkTolax1992.csv", quote = FALSE, row.names = FALSE, na = "NA")
+
+# Create a csv file of the (subset) 1988 data frame and save it to the 'dataset' directory
+write.csv(jfk_lax_1988, file = "../datasets/jfk-lax_1988/jfkTolax1988.csv", quote = FALSE, row.names = FALSE, na = "NA")
+
 
--- week2project/R_workfiles/jfk-lax_88-08.R
+++ week2project/R_workfiles/jfk-lax_88-08.R
@@ -8,10 +8,29 @@
 # Create your own data frame and carry out your own data analysis.
 
 # The task:
+# (for years 1988, 1992, 1996, 2000, 2004 and 2008)
+
+# To show the change of number of flights with origin JFK and destination LAX
+# over a period from 1988 to 2008 in 4 year steps.
 # To compare the change of departure and arrival delays for flights
 # departing from JFK and arriving at LAX from the years 1988 to 2008
-# in 3 year steps.
+# in 4 year steps.
 
 # Read in each years dataset as a data frame
 # 1988
- variableName <- read.csv("../datasets/jfk-lax_1988/...")
+jfk_lax_1988 <- read.csv("../datasets/jfk-lax_1988/jfkTolax1988.csv")
+# 1992
+jfk_lax_1992 <- read.csv("../datasets/jfk-lax_1992/jfkTolax1992.csv")
+# 1996
+jfk_lax_1996 <- read.csv("../datasets/jfk-lax_1996/jfkTolax1996.csv")
+# 2000
+jfk_lax_2000 <- read.csv("../datasets/jfk-lax_2000/jfkTolax2000.csv")
+# 2004
+jfk_lax_2004 <- read.csv("../datasets/jfk-lax_2004/jfkTolax2004.csv")
+# 2008
+jfk_lax_2008 <- read.csv("../datasets/jfk-lax_2008/jfkTolax2008.csv")
+
+
+head(jfk_lax_1988)
+rm(jfk_lax_1988)
+
